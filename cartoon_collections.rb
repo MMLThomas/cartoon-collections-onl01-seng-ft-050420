06816f6dda44e51432586d036a92039c11697548
michael thomas <michael.thomas2014@gmail.com>
2020-05-11 04:12:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(names_array)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.select do |call|
+    "#{call.uppercase}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
