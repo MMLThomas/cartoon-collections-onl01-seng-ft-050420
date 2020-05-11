58807ec2d9da4aa8e7a4c01233ccdc9de1944001
michael thomas <michael.thomas2014@gmail.com>
2020-05-11 04:12:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
   planeteer_calls.select do |call|
-    "#{call.uppercase}!"
+    "#{call.upcase}!"
   end
 end
 
