52b1be411ea1f45985af7f881050dc4223414872
michael thomas <michael.thomas2014@gmail.com>
2020-05-11 04:46:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,8 @@ def long_planeteer_calls(calls)# code an argument here
   longer_than_four
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array_strings)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array_strings.detect 
 end
